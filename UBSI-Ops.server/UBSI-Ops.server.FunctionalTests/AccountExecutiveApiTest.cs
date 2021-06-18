437b773dd055c1183ef448f91a4f574638adedf6
DESKTOP-J0CJF6K\GH <rdupra.globagility@gmail.com>
2021-06-18 18:12:47 +0800

    feat(#28): Create test for specific account executives

--- UBSI-Ops.server/UBSI-Ops.server.FunctionalTests/AccountExecutiveApiTest.cs
+++ UBSI-Ops.server/UBSI-Ops.server.FunctionalTests/AccountExecutiveApiTest.cs
@@ -32,5 +32,23 @@ namespace UBSI_Ops.server.FunctionalTests
 
             accountExecutives.Items.Should().NotBeNull();
         }
+        [Fact]
+        public async Task ShouldRetrieveSpecificAccountExecutive()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("/api/account-executive/606006");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var accountExecutive = JsonSerializer.Deserialize<AccountExecutiveDto>(responseContent, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+
+            accountExecutive.Code.Should().Be("606006");
+            accountExecutive.AreaCode.Should().Be("DAG");
+        }
     }
 }
