e0f63241200c415fdd2abe520476dc3f0b0bef1b
DESKTOP-J0CJF6K\GH <rdupra.globagility@gmail.com>
2021-06-25 16:34:32 +0800

    feat(#32): Override the password validator with md5

---
+++ UBSI-Ops.server/UBSI-Ops.server/Migrations/20210625080538_UpdateUserPasswordlength.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UBSI_Ops.server.Migrations
+{
+    public partial class UpdateUserPasswordlength : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AU_PASSWORD",
+                table: "EZ_USERS",
+                type: "VARCHAR2(32)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR2(30)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AU_PASSWORD",
+                table: "EZ_USERS",
+                type: "VARCHAR2(30)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR2(32)");
+        }
+    }
+}
