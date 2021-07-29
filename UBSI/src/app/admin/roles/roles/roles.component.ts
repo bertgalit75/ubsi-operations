655eab2c9b06bf9098b88a1ab3b31c5afc366742
DESKTOP-J0CJF6K\GH <rdupra.globagility@gmail.com>
2021-07-29 14:26:19 +0800

    feat(#36): Show list of IOs

--- UBSI/src/app/admin/roles/roles/roles.component.ts
+++ UBSI/src/app/admin/roles/roles/roles.component.ts
@@ -40,6 +40,6 @@ export class RolesComponent implements OnInit {
     const currentSort = sort.find((item) => item.value !== null);
     const sortField = (currentSort && currentSort.key) || null;
     const sortOrder = (currentSort && currentSort.value) || null;
-    this.list(pageIndex, pageSize, sortField, sortOrder);
+    this.list(pageIndex - 1, pageSize, sortField, sortOrder);
   }
 }
