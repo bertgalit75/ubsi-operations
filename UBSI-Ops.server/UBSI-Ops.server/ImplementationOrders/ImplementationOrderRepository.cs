f734f1407a384dc3a0cceec45b21da31489f0f99
DESKTOP-J0CJF6K\GH <rdupra.globagility@gmail.com>
2021-08-03 11:12:17 +0800

    feat(#41): validation of Agency

--- UBSI-Ops.server/UBSI-Ops.server/ImplementationOrders/ImplementationOrderRepository.cs
+++ UBSI-Ops.server/UBSI-Ops.server/ImplementationOrders/ImplementationOrderRepository.cs
@@ -7,15 +7,31 @@ namespace UBSI_Ops.server.Services
 {
     public class ImplementationOrderRepository : Repository, IImplementationOrderRepository
     {
-        public ImplementationOrderRepository(OperationContext context)
+        private readonly IMediaAgencyRepository _mediaAgencyRepository;
+
+        public ImplementationOrderRepository(OperationContext context, IMediaAgencyRepository mediaAgencyRepository)
             : base(context)
         {
+            _mediaAgencyRepository = mediaAgencyRepository;
         }
 
         public async Task Add(ImplementationOrder implementationOrder)
         {
-            _context.ImplementationOrders.Add(implementationOrder);
-            await _context.SaveChangesAsync();
+            if (implementationOrder.AgencyCode != null)
+            {
+                var agency = await _mediaAgencyRepository.View(implementationOrder.AgencyCode);
+
+                if (agency != null)
+                {
+                    _context.ImplementationOrders.Add(implementationOrder);
+                    await _context.SaveChangesAsync();
+                }
+            }
+            else
+            {
+                _context.ImplementationOrders.Add(implementationOrder);
+                await _context.SaveChangesAsync();
+            }
         }
     }
 }
